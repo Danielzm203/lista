b1a177f51eb3c9d296e3cd04ad7c8f287de7ab37
Daniel zm203 <42298039+Danielzm203@users.noreply.github.com>
2023-12-04 21:09:22 +0100

    version suma

---
+++ lista.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class lista {
+    public static void main(String[]args){
+        int num1;
+        int num2;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("1.Suma");
+        System.out.println("2.Resta");
+        System.out.println("3.Multiplicacion");
+        System.out.println("4.Division");
+        System.out.println("5.Salir");
+        int op;
+        System.out.println("Ingresa una opcion:");
+        op= sc.nextInt();
+        System.out.println("Dame un numero:");
+        num1=sc.nextInt();
+        System.out.println("Dame otro numero:");
+        num2=sc.nextInt();
+        switch (op){
+            case 1:
+                int suma=num1+num2;
+                System.out.println("El resultado es: "+suma);
+		break;
+        }
+    }
+}
